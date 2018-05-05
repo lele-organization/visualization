e2ad9e75413ebd72878f62796bb7227d7435afa3
ShazLeLe <donghuanjie1995@163.com>
2018-05-05 18:04:58 +0800

    实时报警事件

--- js/index.js
+++ js/index.js
@@ -19,48 +19,70 @@ $(function () {
 
       var option = {
         tooltip: {
-          trigger: 'item',
-          formatter: "{a} <br/>{b}: {c} ({d}%)"
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
         },
         // legend: {
-        //   orient: 'vertical',
-        //   x: 'left',
-        //   data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+        //   orient: 'horizontal',
+        //   left:'center',
+        //   // top:'bottom',
+        //   bottom:'0',
+        //   data:['重要','紧急','一般'],
+        //   itemGap:20,
+        //   itemHeight:12,
+        //   itemWidth:12,
+        //   selectedMode:false,
+        //   textStyle:{
+        //       color:'#fff',
+        //       fontSize:12
+        //   }
         // },
         series: [
-          {
-            name:'访问来源',
-            type:'pie',
-            radius: ['50%', '70%'],
-            avoidLabelOverlap: false,
-            label: {
-                normal: {
-                  show: false,
-                  position: 'center'
+            {
+                name:'实时报警事件统计',
+                type:'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    normal: {
+                        position: 'outside',
+                        fontSize:18,
+                        // fontSize:14,
+                        color:'#fff',
+                        formatter: "{d}%"
+                    },
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            // fontSize: '30',
+                            // fontWeight: 'bold'
+                        }
+                    },
                 },
-                emphasis: {
-                  show: true,
-                  textStyle: {
-                      fontSize: '10',
-                      fontWeight: 'bold'
-                  }
-                }
-            },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            data:[
-                {value:335, name:'直接访问'},
-                {value:310, name:'邮件营销'},
-                {value:234, name:'联盟广告'},
-                {value:135, name:'视频广告'},
-                {value:1548, name:'搜索引擎'}
-            ]
-          }
+                labelLine: {
+                    normal: {
+                        show: true,
+                        length:10,
+                        length2:10,
+                    }
+                },
+                data:[
+                    {value:65, name:'重要'},
+                    {value:10, name:'紧急'},
+                    {value:25, name:'一般'}
+                ],
+                color:['#f08400', '#ff0000','#ffdb5f'],
+
+                // data:[
+                //   {value:10, name:'紧急'},
+                //   {value:65, name:'重要'},
+                //   {value:25, name:'一般'}
+                // ],
+                // color:['#ff0000','#f08400', '#ffdb5f'],
+                
+            }
         ]
-      }
+    };
 
       myChart.setOption(option);
     }
